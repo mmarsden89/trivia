3c81a15c910ae5d73fdc6b29782c05c03ac721f5
mmarsden89 <mattmarsdendev@gmail.com>
2020-10-28 17:32:42 -0700

    add header component

---
+++ src/components/header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.scss";
+
+const Header = () => {
+  return (
+    <div className="header-container">
+      <h1>Trvia</h1>
+    </div>
+  );
+};
+
+export default Header;
