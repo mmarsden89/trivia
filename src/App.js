bf8d7d6c5e73081f0f6b46b1c9db0bd6eda195eb
mmarsden89 <mattmarsdendev@gmail.com>
2020-10-28 19:37:22 -0700

    separate menu and playarea files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.scss";
 import Header from "./components/header/Header.js";
 import PlayArea from "./components/playarea/PlayArea.js";
+import Menu from "./components/menu/Menu.js";
 
 function App() {
   return (
@@ -8,7 +9,8 @@ function App() {
       <header className="App-header">
         <Header />
       </header>
-      <PlayArea />
+      {/* <PlayArea /> */}
+      <Menu />
     </div>
   );
 }
