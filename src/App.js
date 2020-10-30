f44b43aae2ce09a7cbc6781a4c63159f058326c7
mmarsden89 <mattmarsdendev@gmail.com>
2020-10-30 16:37:00 -0700

    add menu toggle

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import "./App.scss";
+import React, { useState } from "react";
 import Header from "./components/header/Header.js";
 import PlayArea from "./components/playarea/PlayArea.js";
 import Menu from "./components/menu/Menu.js";
 import Footer from "./components/footer/Footer.js";
 
 function App() {
+  const [menu, setMenu] = useState(false);
+  const toggleMenu = () => {
+    setMenu(!menu);
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <Header />
+        <Header toggleMenu={toggleMenu} />
       </header>
       <div className="body-container">
-        <Menu />
+        {menu && <Menu />}
         <PlayArea />
       </div>
       <Footer />
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,12 @@
 import React from "react";
 import "./Header.scss";
 
-const Header = () => {
+const Header = (props) => {
   return (
     <div className="header-container">
+      <div className="button menu" onClick={props.toggleMenu}>
+        menu
+      </div>
       <h1>Trivia</h1>
     </div>
   );
