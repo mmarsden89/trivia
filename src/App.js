1cb61f200f0e73a6e3a00b0e3b9939fc12a52ba2
mmarsden89 <mattmarsdendev@gmail.com>
2020-11-01 19:30:17 -0800

    move consolelogger into new helpers directory

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import questions from "./questions.json";
 import questionseasy from "./questionseasy.json";
 import questionsmedium from "./questionsmedium.json";
 import questionshard from "./questionshard.json";
+import consoleLogger from "./helpers/consolelogger";
 
 function App() {
   const [menu, setMenu] = useState("");
@@ -85,23 +86,6 @@ function App() {
       handleCookieInfo(type);
     }
   };
-  if (!consoled) {
-    const imageStyle = [
-      'background-image: url("https://thumbs.gfycat.com/ImmaculateImpassionedKitten-max-1mb.gif")',
-      "background-size: cover",
-      "padding: 150px 300px",
-      "width: 100px;",
-      "height: 50px;",
-    ].join(";");
-    console.log("%c ", imageStyle);
-
-    console.log(
-      "%cThis could be us squashing bugs together if you hire me! %c\nportfolio \nhttps://mmarsden89.github.io\ngithub \nhttps://github.com/mmarsden89 \nlinkedin \nhttps://www.linkedin.com/in/matthewjmarsden/",
-      "color:white;font-size:12px;background-color:green",
-      "color: blue; font-weight: 900"
-    );
-    setConsoled(true);
-  }
 
   const handleQuestionSet = (type) => {
     if (type === "default") {
@@ -122,6 +106,7 @@ function App() {
       setCookie(true);
       setCookieNum(JSON.parse(document.cookie.split("=")[1]).score);
     }
+    consoleLogger(setConsoled, consoled);
     setNewGame(false);
   }, [cookie, cookieNum, questionSet]);
 
